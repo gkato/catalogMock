dc8d193caa6f6253aa1c1001e5c04bdba10e387d
Guilherme Kato <guilherme.kato@wal-mart.com>
2014-05-16 09:55:02 -0300

    Mock negative

--- app/controllers/catalogs_controller.rb
+++ app/controllers/catalogs_controller.rb
@@ -1,7 +1,8 @@
 class CatalogsController < ApplicationController
 
   def index
-     json = {productId:100220404,collections:[1],featuredCollections:[]}
+     collections = params[:product] == "20" ? [] : [1]
+     json = {productId:100220404,collections:[collections],featuredCollections:[]}
      render :json => json.to_json
   end
 end

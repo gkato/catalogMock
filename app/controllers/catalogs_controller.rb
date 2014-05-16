f210a7d3ba9d08da0052fe59ba91d5cc7385ae38
Willians Romera <willians.rodrigues@wal-mart.com>
2014-05-16 11:31:01 -0300

    Problema com colchetes

--- app/controllers/catalogs_controller.rb
+++ app/controllers/catalogs_controller.rb
@@ -2,7 +2,7 @@ class CatalogsController < ApplicationController
 
   def index
      collections = params[:product] == "20" ? [] : [1]
-     json = {productId:100220404,collections:[collections],featuredCollections:[]}
+     json = {productId:100220404,collections:collections,featuredCollections:[]}
      render :json => json.to_json
   end
 end
